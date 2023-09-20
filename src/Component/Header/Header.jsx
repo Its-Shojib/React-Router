2a46a1715e215029753396e24945f472f66f53e0
Its_Shojib <mdshojib922@gmail.com>
2023-09-21 01:00:24 +0600

    All Router Added

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
         <div>
             <h2>My Website</h2>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/contact'>Contact</Link>
-                <Link to='/users'>Users</Link>
-                <Link to='/posts'>Posts</Link>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
             </nav>
         </div>
     );
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 const Home = () => {
+    let navigation = useNavigation();
+    let location = useLocation();
+    console.log(location);
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state ==='loading' ?
+                <p>Loading...</p> : <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
